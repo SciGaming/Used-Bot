0f19a4cde734b5979257eb05684425b6b7b95874
SciGaming <49855624+SciGaming@users.noreply.github.com>
2019-04-22 13:08:46 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord.ext.commands import has_permissions
+import asyncio
+from itertools import cycle
+import time
+import youtube_dl
+
+my_token = 
